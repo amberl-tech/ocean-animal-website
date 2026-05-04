91aa9a522eab3aff5716844c1d937dbe147abf95
amberl-tech <Hca82@icloud.com>
2026-05-03 20:23:06 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const message = document.getElementById("welcomeMessage");
+  if (message) {
+    message.textContent = "Welcome to the Ocean Animals Website!";
+  }
+});
